daadf8cf345f62847af9281342a16c2e62a4fbd8
Nasrul Alawy <nasrul.expandana@gmail.com>
2021-09-28 06:21:27 +0800

    add route put,patch,delete

---
+++ bin/dchisel.dart
@@ -0,0 +1,3 @@
+void main(List<String> args) {
+  print('DChisel Dart REST API Framework');
+}
--- lib/src/dchiselServer.dart
+++ lib/src/dchiselServer.dart
@@ -14,9 +14,14 @@ var _password = '';
 
 var _routeGet = [];
 var _routePost = [];
-var _dbFunctionGet = [];
+var _routePut = [];
+var _routeDelete = [];
+var _routePatch = [];
 List<Function> _dataHandlerGet = [];
 List<Function> _dataHandlerPost = [];
+List<Function> _dataHandlerPut = [];
+List<Function> _dataHandlerDelete = [];
+List<Function> _dataHandlerPatch = [];
 
 List<Map<String, dynamic>>? base = [];
 
@@ -31,6 +36,21 @@ class DChisel {
     _dataHandlerPost.add(data);
   }
 
+  void routePut(String route, Function data) {
+    _routePut.add(route);
+    _dataHandlerPut.add(data);
+  }
+
+  void routeDelete(String route, Function data) {
+    _routeDelete.add(route);
+    _dataHandlerDelete.add(data);
+  }
+
+  void routePatch(String route, Function data) {
+    _routePatch.add(route);
+    _dataHandlerPatch.add(data);
+  }
+
   Future<void> serve({serverHost, serverPort}) async {
     var routes = Router().plus;
 
@@ -40,6 +60,15 @@ class DChisel {
       }
       for (var i = 0; i < _routePost.length; i++) {
         routes.post(_routePost[i], _dataHandlerPost[i]);
+      }
+      for (var i = 0; i < _routePut.length; i++) {
+        routes.put(_routePut[i], _dataHandlerPut[i]);
+      }
+      for (var i = 0; i < _routeDelete.length; i++) {
+        routes.delete(_routeDelete[i], _dataHandlerDelete[i]);
+      }
+      for (var i = 0; i < _routePatch.length; i++) {
+        routes.delete(_routePatch[i], _dataHandlerPatch[i]);
       }
 
       return routes;
