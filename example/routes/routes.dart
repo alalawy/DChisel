292e3d7bc4858ceddd01b9d0e92b32c5578c2cbd
Nasrul Alawy <nasrul.expandana@gmail.com>
2021-09-28 07:48:58 +0800

    add comments to example route

--- example/routes/routes.dart
+++ example/routes/routes.dart
@@ -11,27 +11,34 @@ class Routes {
 
     DChisel().routeGet('/', (Request request) {
       return DChiselDB().getAll('users');
-    });
+    }); // GET ALL DATA USERS FROM DB
+
     DChisel().routeGet('/users', (Request request) {
       return DChiselDB().getAll('users');
-    });
+    }); // GET ALL DATA USERS FROM DB
+
     DChisel().routeGet('/users/<name>', (Request request, String name) {
       return DChiselDB()
           .getOption('users', colum: 'email', where: ['name', '%$name%']);
-    });
+    }); // GET ONE DATA USERS FROM DB EITH FILTER BY NAME
+
     DChisel().routePost('/users/add', (Request request) async {
       return DChiselDB().create('users', data: await request.body.asJson);
-    });
+    }); // CREATE NEW USERS
+
     DChisel().routeDelete('/users/<id>', (Request request, String id) {
       return DChiselDB().deleteOption('users', where: ['id', int.parse(id)]);
-    });
+    }); // DELETE USERS BY ID
+
     DChisel().routeDelete('/deleteall/users', (Request request) {
       return DChiselDB().deleteAll('users');
-    });
+    }); // DELETE ALL USERS
+
     DChisel().routePut('/users/<id>', (Request request, String id) async {
       return DChiselDB().update('users',
           data: await request.body.asJson, where: ['id', int.parse(id)]);
-    });
+    }); // UPDATE USERS BY ID
+
     //ROUTES
   }
 }
